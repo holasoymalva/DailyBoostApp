d0fab12f5800c6dbb845ab6cb07f88e136181404
holasoymalva <alquimista3301@gmail.com>
2025-01-14 21:00:05 -0600

    Initial Commit

---
+++ DailyBoostApp Watch App/DailyBoostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyBoostAppApp.swift
+//  DailyBoostApp Watch App
+//
+//  Created by malva on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyBoostApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyBoostApp Watch AppTests/DailyBoostApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyBoostApp_Watch_AppTests.swift
+//  DailyBoostApp Watch AppTests
+//
+//  Created by malva on 14/01/25.
+//
+
+import Testing
+@testable import DailyBoostApp_Watch_App
+
+struct DailyBoostApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
